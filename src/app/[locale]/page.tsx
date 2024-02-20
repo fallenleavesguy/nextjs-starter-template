aa35f55a0f6d586e606a03950d0732981c54cc87
fallenleavesguy <9261562+fallenleavesguy@users.noreply.github.com>
2024-02-21 00:49:07 +0800

    fix: 支持shadcn ui

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,53 @@
 import {useTranslations} from 'next-intl';
+
+import * as React from "react"
+
+import { Card, CardContent } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+ 
+export function ButtonDemo() {
+  return <Button>Button</Button>
+}
+
+export function CarouselSpacing() {
+  return (
+    <Carousel className="w-full max-w-sm">
+      <CarouselContent className="-ml-1">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="pl-1 basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-2xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
+
  
 export default function Index() {
   const t = useTranslations('Index');
-  return <h1>{t('title')}</h1>;
+  return (
+    <div>
+      <CarouselSpacing />
+      <div className="px-4">
+        <h1>{t('title')}</h1>
+        <ButtonDemo />
+      </div>
+    </div>
+  );
 }
